6d2e9fcb74982a664c08866ecbe051ea2722919c
Jenny <jfperham@gmail.com>
2022-05-01 22:16:53 -0700

    API practice: routes, endpoints, Flask, also adding url image to static website with hrml

---
+++ api_practice_100dayscode/100d_code_cafe_API/main.py
@@ -0,0 +1,93 @@
+# 1. set up a virtual environment
+# 2. Install Flask: `pip install Flask`
+# 3. in terminal: `export FLASK_APP=main.py`
+# 4. then: `flask run`
+
+from flask import Flask, jsonify, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+import random
+
+app = Flask(__name__)
+
+##Connect to Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+##Cafe TABLE Configuration
+class Cafe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), unique=True, nullable=False)
+    map_url = db.Column(db.String(500), nullable=False)
+    img_url = db.Column(db.String(500), nullable=False)
+    location = db.Column(db.String(250), nullable=False)
+    seats = db.Column(db.String(250), nullable=False)
+    has_toilet = db.Column(db.Boolean, nullable=False)
+    has_wifi = db.Column(db.Boolean, nullable=False)
+    has_sockets = db.Column(db.Boolean, nullable=False)
+    can_take_calls = db.Column(db.Boolean, nullable=False)
+    coffee_price = db.Column(db.String(250), nullable=True)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+    
+
+## HTTP GET - Read Record
+@app.route("/random",methods=["GET"])
+def get_random_cafe():
+    cafes = Cafe.query.all() # this will get all instances of cafe, I think
+    # cafes = db.session.query(Cafe).all()  # answer provided by course
+    cafe = random.choice(cafes) # gets a random cafe
+    return jsonify({
+            'id': cafe.id,
+            'name' :cafe.name,
+            'map_url': cafe.map_url,
+            'img_url': cafe.img_url,
+            'location': cafe.location,
+            'seats' : cafe.seats,
+            'has_toilet' : cafe.has_toilet,
+            'has_wifi' : cafe.has_wifi,
+            'has_sockets' : cafe.has_sockets,
+            'can_take_calls': cafe.can_take_calls,
+            'coffee_price': cafe.coffee_price
+
+    }), 200
+
+@app.route("/cafes", methods=["GET"])
+def get_all_cafes():
+    cafes = Cafe.query.all()
+
+    # one-line option, using .to_dict() method and list comprehension:
+    # however, gave me an error?
+    # return jsonify(cafes=[cafe.to_dict() for cafe in cafes]), 200
+
+    cafe_list = []
+    for cafe in cafes:
+        cafe_list.append({
+            'id': cafe.id,
+            'name' :cafe.name,
+            'map_url': cafe.map_url,
+            'img_url': cafe.img_url,
+            'location': cafe.location,
+            'seats' : cafe.seats,
+            'has_toilet' : cafe.has_toilet,
+            'has_wifi' : cafe.has_wifi,
+            'has_sockets' : cafe.has_sockets,
+            'can_take_calls': cafe.can_take_calls,
+            'coffee_price': cafe.coffee_price
+    })
+    return jsonify(cafe_list), 200
+
+## HTTP POST - Create Record
+
+
+## HTTP PUT/PATCH - Update Record
+
+## HTTP DELETE - Delete Record
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
