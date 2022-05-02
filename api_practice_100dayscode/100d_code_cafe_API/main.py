3cb0709977d4ab539992e8f5215307fd5596e665
Jenny <jfperham@gmail.com>
2022-05-02 08:06:22 -0700

    worked on API endpoint problems

--- api_practice_100dayscode/100d_code_cafe_API/main.py
+++ api_practice_100dayscode/100d_code_cafe_API/main.py
@@ -3,6 +3,8 @@
 # 3. in terminal: `export FLASK_APP=main.py`
 # 4. then: `flask run`
 
+# Questions: 
+# /search endpoint: how to search for cafes in a location using params?
 from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 import random
@@ -34,7 +36,6 @@ class Cafe(db.Model):
 def home():
     return render_template("index.html")
     
-
 ## HTTP GET - Read Record
 @app.route("/random",methods=["GET"])
 def get_random_cafe():
@@ -81,7 +82,51 @@ def get_all_cafes():
     })
     return jsonify(cafe_list), 200
 
+# response = request.get(path, params=query_params)
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# data = response.json()
+
+# Trying to search for one cafe by location
+# @app.route("/search", methods=['GET'])
+# def search_by_location():
+#     response = request.get(http://localhost:5000/search, params=query_params)
+#     query_params = {
+#         'location': location
+#     }
+#     cafes = Cafe.query.all() # redundent with response above.
+#     cafes_at_location = []
+#     for cafe in cafes:
+#         if cafe.location == query_params['location']: # how to do this?
+#             cafes_at_location.append({
+#             'id': cafe.id,
+#             'name' :cafe.name,
+#             'map_url': cafe.map_url,
+#             'img_url': cafe.img_url,
+#             'location': cafe.location,
+#             'seats' : cafe.seats,
+#             'has_toilet' : cafe.has_toilet,
+#             'has_wifi' : cafe.has_wifi,
+#             'has_sockets' : cafe.has_sockets,
+#             'can_take_calls': cafe.can_take_calls,
+#             'coffee_price': cafe.coffee_price
+#     })
+#     return jsonify(cafes_at_location), 200
+    
 ## HTTP POST - Create Record
+@app.route("", methods=['POST'])
+def add_a_cafe():
+    request_body = request.get_json()
+    new_cafe = Cafe(id=request_body['id'],name=request_body['name'], map_url=request_body['map_url'],
+    img_url=request_body['img_url'], location=request_body['location'], seats=request_body['seats'],
+    has_toilet=request_body['has_toilet'], has_wife=request_body['has_wifi'], 
+    has_sockets=request_body['has_sockets'], can_take_calls=request_body['can_take_calls'],
+    coffee_price=request_body['coffee_price'])
+    # How can this be done without typing in all of the above??  
+    db.session.add(new_cafe)
+    db.session.commit()
+    return {
+        'response': f'Sucessfully added a new cafe: {new_cafe.name}. The cafe id is: {new_cafe.id}'
+    }, 201
 
 
 ## HTTP PUT/PATCH - Update Record
